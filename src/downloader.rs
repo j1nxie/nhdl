29fa1b0765f4757e0df81df561b3b33a13fbe696
j1nxie <52886388+j1nxie@users.noreply.github.com>
2021-12-13 12:13:08 +0700

    [docs] add documentation to downloader.rs

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,3 +1,8 @@
+/*
+ * downloader function for nhdl
+ * take a Vec<String> of urls, a String id as folder name and a reqwest Client
+ * output downloaded files from given urls asynchronously
+*/
 use std::sync::Arc;
 use regex::Regex;
 use futures::future::join_all;
@@ -6,9 +11,10 @@ use tokio::task::JoinHandle;
 use reqwest::Client;
 
 pub async fn downloader(paths: Vec<String>, id: String, client: Client) {
-    let sem = Arc::new(Semaphore::new(10));
-    let mut tasks: Vec<JoinHandle<Result<(), ()>>> = vec![];
+    let sem = Arc::new(Semaphore::new(10)); // limit max amount of threads so downloads don't break
+    let mut tasks: Vec<JoinHandle<Result<(), ()>>> = vec![]; // initialize empty vector for list of tasks
     for path in paths {
+        // clone path and id to fix ownership problems
         let path = path.clone();
         let id = id.clone();
         let send_fut = client.get(&path).send();
@@ -16,9 +22,9 @@ pub async fn downloader(paths: Vec<String>, id: String, client: Client) {
 
         tasks.push(tokio::spawn(async move {
             let _permit = permit;
-            match send_fut.await {
-                Ok(resp) => match resp.bytes().await {
-                    Ok(stream) => match image::load_from_memory(&stream) {
+            match send_fut.await { // get html response
+                Ok(resp) => match resp.bytes().await { // get bytes as a file stream
+                    Ok(stream) => match image::load_from_memory(&stream) { // load bytes into memory
                         Ok(img) => {
                             let page_re = Regex::new(r"(\w+\.)+\w+$").unwrap();
                             let page_caps = page_re.captures(&path).unwrap();

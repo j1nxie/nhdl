fed2e9b6663b6b64a5e9e11db1e9ddd052e807d7
j1nxie <52886388+j1nxie@users.noreply.github.com>
2021-12-19 12:11:54 +0700

    [feat] wip: text input support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
 use std::path::Path;
-use std::fs;
-
-use select::document::Document;
+use std::fs::{self, File};
+use std::io::BufReader;
+use std::io::prelude::*;
 
 use reqwest::Client;
 
@@ -19,16 +19,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     // get url from user input
-    let mut input = matches.value_of("INPUT").unwrap().to_string();
+    let input = matches.value_of("INPUT").unwrap().to_string();
+    let mut list = vec![];
     loop {
         match &input.trim().parse::<u32>() {
             Ok(id) => {
-                input = format!("https://nhentai.net/g/{}/", id);
+                list.push(format!("https://nhentai.net/g/{}/", id));
                 break;
             },
             Err(_) => {
                 match &input.trim().contains("https://nhentai.net/g/") {
-                    true => break,
+                    true => {
+                        list.push(input);
+                        break;
+                    },
                     false => println!("[error] invalid url or id!")
                 }
             }
@@ -37,31 +41,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // get html source from reqwest
     let client = Client::builder().build()?;
-    let response = client.get(&input).send().await?;
-    let body = response.text().await?;
-    let document = Document::from(body.as_str());
+    for doujin in list {
+        let document = html::get_document(doujin, client.clone()).await;
 
-    // get metadata 
-    let mut title: nhentai::NH = Default::default();
-    title.get_title(document.clone());
-    title.get_id(document.clone());
-    title.get_tags(document.clone());
-    title.get_page(document.clone());
-    title.get_gallery(document.clone());
-    title.print_status();
+        // get metadata 
+        let mut title: nhentai::NH = Default::default();
+        title.get_title(document.clone());
+        title.get_id(document.clone());
+        title.get_tags(document.clone());
+        title.get_page(document.clone());
+        title.get_gallery(document.clone());
+        title.print_status();
 
-    // initialize download directory
-    let dir = format!("./{}", title.id);
-    if !Path::new(&dir).exists() {
-        fs::create_dir(dir)?;
+        // initialize download directory
+        let dir = format!("./{}", title.id);
+        if !Path::new(&dir).exists() {
+            fs::create_dir(dir)?;
+        }
+
+        // initialize download links vector
+        let mut paths = vec![];
+        for i in 1..=title.pages.parse::<u8>().unwrap() {
+            paths.push(format!("https://i.nhentai.net/galleries/{}/{}.jpg", title.gallery_id, i));
+        }
+        downloader::downloader(paths, title.id, client.clone()).await;
     }
-
-    // initialize download links vector
-    let mut paths = vec![];
-    for i in 1..=title.pages.parse::<u8>().unwrap() {
-        paths.push(format!("https://i.nhentai.net/galleries/{}/{}.jpg", title.gallery_id, i));
-    }
-    downloader::downloader(paths, title.id, client).await;
 
     Ok(())
 }
